67d42acf7e6829f1c4379ec8cd33d5271963a76f
Fernando Silva <f_silvadealmeida@hotmail.com>
2023-02-15 18:06:04 -0300

    netflixclone

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
 import './App.css';
+import Tmdb from "./Tmdb";
+import MovieRow from "./components/MovieRow";
+import FeaturedMovie from "./components/FeaturedMovie";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+/* eslint import/no-anonymous-default-export: [2, {"allowArrowFunction": true}] */              //[TRATA ERROS EXPORT ANONIMO]
+export default () =>{
+
+  const [movieList, setMovieList] = useState([]);
+  const [FeaturedData, setFeaturedData] = useState(null);
+
+  useEffect(() =>{
+    const loadall = async () =>{
+      //pegando a lista total
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+      
+      //pegando o Featured
+      let originals = list.filter(i=>i.slug === 'originais');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      //console.log(chosenInfo);
+      setFeaturedData(chosenInfo); 
+    }
+    loadall();
+  },[])
+
+  return(
+    <div className="page">
+      {
+        FeaturedData && <FeaturedMovie item={FeaturedData}/>
+      }
+
+      <section className="lists">
+        {
+          movieList.map((item, key) => (
+            <div key={key}>
+              <MovieRow key={key} title={item.title} items={item.items} />
+            </div>
+          ))
+        }
+      </section>
     </div>
-  );
+  )
 }
-
-export default App;
