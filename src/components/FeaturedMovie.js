d8cbc377a8fc7daa81932cf114f35862f1489ca4
FernandoSilva <f_silvadealmeida@hotmail.com>
2023-02-20 14:57:38 -0300

    FINALIZADO

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -9,6 +9,11 @@ export default ({item}) =>{
     let genres = [];
     for(let i in item.genres){
         genres.push( item.genres[i].name);
+    } 
+
+    let description = item.overview;
+    if(description.length > 200){
+        description = description.substring(0,200)+'...';
     }
 
 
@@ -26,7 +31,7 @@ export default ({item}) =>{
                         <div className="featured--yaer">{firstDate.getFullYear()}</div>
                         <div className="featured--seasons">{item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's' : ''}</div>
                     </div>
-                    <div className="featured--description">{item.overview}</div>
+                    <div className="featured--description">{description}</div>
                     <div className="featured--buttons">
                         <a href={`/watch/${item.id}`} className="featured--watchbutton"> ► Assistir</a>
                         <a href={`/list/add/${item.id}`} className="featured--mylistbutton"> + Minha Lista</a>
--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -1,22 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import './MovieRow.css';
+import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 
 /* eslint import/no-anonymous-default-export: [2, {"allowArrowFunction": true}] */                                 //[TRATA ERROS EXPORT ANONIMO]
 export default ({title, items}) =>{
+
+    const [scrollX, setScrollX] = useState(0);
+
+    const handleLeftArrow = () =>{
+        let x = scrollX + Math.round(window.innerWidth / 2);
+        if(x > 0){
+            x = 0;
+        }
+        setScrollX(x);
+    };
+    
+    const hendleRightArrow = () =>{
+        let x = scrollX - Math.round(window.innerWidth / 2);
+        let listW = items.results.length * 150;
+        if((window.innerWidth - listW) > x){
+            x = (window.innerWidth - listW) - 60;
+        }
+        setScrollX(x);
+    };
+
     return(
         <div className="movieRow">
             <h2>{title}</h2>
+                
+            <div className="movieRow--left" onClick={handleLeftArrow}>
+                <NavigateBeforeIcon  style={{fontSize: 50}}/> 
+            </div>
+            <div className="movieRow--right" onClick={hendleRightArrow}>
+                <NavigateNextIcon style={{fontSize: 50}}/>
+            </div> 
+
             <div className="movieRow--listarea">
-                <div className="movieRow--list">
+                <div className="movieRow--list" style={{
+                    marginLeft: scrollX,
+                    width: items.results.length * 150
+                }}>
                     {items.results.length > 0 && items.results.map((item, key) =>(
                         <div key={key} className="movieRow--item">
                             <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title}/>
                         </div>
                     ))}
                 </div>
-
-               
             </div>
+
+            
         </div>
     )
 }
